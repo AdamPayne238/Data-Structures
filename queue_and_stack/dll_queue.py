8120500f6d43c3d9646c003e4cbfad66f84a60d4
Adam Payne <ajpayne238@gmail.com>
2020-02-24 17:31:58 -0500

    working through stacks and queues

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -4,20 +4,23 @@ from doubly_linked_list import DoublyLinkedList
 
 # Queues follow the First-in-First-Out principle (FIFO)
 # First one in line at the movies is the first to buy a ticket and enjoy the movie
-# enqueue - adds an element to the end of the queue
-# dequeue - removes the element at the beginning of the queue
+
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
+    # enqueue - adds an element to the end of the queue
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
+    # dequeue - removes the element at the beginning of the queue
     def dequeue(self):
+        # remove_from_head
         pass
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -3,23 +3,25 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-
 # Stacks follow Last-in-First-Out principle (LIFO)
 # Just like stacking coins, the last coin we put on top is the one that is the first to be removed from the stack later
-# Push - add an element to the top of the stack
-# Pop  - removes the element at the top of the stack
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
+    # Push - add an element to the top of the stack
     def push(self, value):
-        self.size.append(value)
+        self.storage.add_to_tail(value)
+        self.size += 1
 
+    # Pop  - removes the element at the top of the stack
     def pop(self):
         pass
+    # remove_from_head?
 
     def len(self):
-        pass
+        return self.size
