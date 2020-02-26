cb2b0fc744665df6f40a9cb4c87b2221185be01b
Adam Payne <ajpayne238@gmail.com>
2020-02-26 14:09:24 -0500

    set up pseudo code for binary_search_tree.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -3,6 +3,30 @@ sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
 
+# Binary Search Tree is a node-based binary tree data structure which has the following properties:
+# The left subtree of a node contains only nodes with keys lesser than the node’s key.
+# The right subtree of a node contains only nodes with keys greater than the node’s key.
+# The left and right subtree each must also be a binary search tree.
+
+# Insert Value
+# If no root node, insert as root node (first in tree)
+# If node being inserted is greater than root node
+    # Move right
+# If node being inserted is less than root node
+    # Move left
+# If node has traversed and no more nodes to compare. Insert here.
+
+# Find Value
+# If no node at root: return false
+# Compare value to root
+# if smaller:
+    # Go left. Look at node there
+# If Greater or ==:
+    # Go right.
+
+# Get Max
+# If no right child. Return this value
+# Otherwise continue right in the tree to find the largest node
 
 class BinarySearchTree:
     def __init__(self, value):
